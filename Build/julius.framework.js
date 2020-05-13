5ea8c692cbd4d2249dea7ddab10019effa68cc47
Samir Ghosh <hello@samir.tech>
2020-05-12 17:33:45 -0700

    update: rl changes

--- Build/julius.framework.js
+++ Build/julius.framework.js
@@ -1299,7 +1299,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 3236368;
+STATICTOP = STATIC_BASE + 3237056;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3233,7 +3233,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 3236368;
+var STATIC_BUMP = 3237056;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -15588,8 +15588,8 @@ function nullFunc_vjji(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
-Module["wasmTableSize"] = 88930;
-Module["wasmMaxTableSize"] = 88930;
+Module["wasmTableSize"] = 88934;
+Module["wasmMaxTableSize"] = 88934;
 function invoke_dd(index, a1) {
  var sp = stackSave();
  try {
